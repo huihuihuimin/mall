8002dbe241f0b668f183fe8d742ee22173567392
你大爷 <632581085@qq.com>
2019-06-14 11:13:19 +0800

    站内信，评论

--- src/main/java/com/nowcoder/controller/SettingController.java
+++ src/main/java/com/nowcoder/controller/SettingController.java
@@ -2,7 +2,6 @@ package com.nowcoder.controller;
 
 import com.nowcoder.service.WendaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Conditional;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,16 +10,23 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import javax.servlet.http.HttpSession;
 
 /**
- * Created by nowcoder on 2016/7/10.
+ * 〈一句话功能简述〉<br>
+ * 〈〉
+ *
+ * @author LSN
+ * @create 2019/6/13
+ * @since 1.0.0
  */
 @Controller
-public class SettingController {
+public class SettingController
+{
     @Autowired
     WendaService wendaService;
 
     @RequestMapping(path = {"/setting"}, method = {RequestMethod.GET})
     @ResponseBody
-    public String setting(HttpSession httpSession) {
+    public String setting(HttpSession httpSession)
+    {
         return "Setting OK. " + wendaService.getMessage(1);
     }
 }
